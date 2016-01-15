dea8393a79fd8ccbf5e2af6a0a7fda4e66fbeed6
Suin Kim <suinkim98@gmail.com>
2016-01-15 08:46:54 +0700

    Update

--- mande/urls.py
+++ mande/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'mande'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<job_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<job_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<job_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- mande/views.py
+++ mande/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+from .models import Job
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the mande index.")
+    latest_job_list = Job.objects.order_by('-pub_date')[:5]
+    context = {'latest_job_list': latest_job_list}
+    return render(request, 'mande/index.html', context)
 
-# Create your views here.
+def detail(request, job_id):
+    job = get_object_or_404(Job, pk=job_id)
+    return render(request, 'mande/detail.html', {'job': job})
+
+def results(request, job_id):
+    response = "You're looking at the results of job %s."
+    return HttpResponse(response % job_id)
+
+def vote(request, job_id):
+    return HttpResponse("You're voting on job %s." % job_id)

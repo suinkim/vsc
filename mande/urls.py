1e6eda077d45fb79ba1dea6ef80f480fe1dce74e
Suin Kim <suinkim98@gmail.com>
2016-01-26 10:30:29 +0700

    Update

--- mande/urls.py
+++ mande/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^index/$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<job_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]

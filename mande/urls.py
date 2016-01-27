48794bc7d3164509cb0ea2407df5f289439cb8ac
Suin Kim <suinkim98@gmail.com>
2016-01-27 14:41:19 +0700

    Update

--- mande/urls.py
+++ mande/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'mande'
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^signup$', views.signup, name='signup'),
     url(r'^index/$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
